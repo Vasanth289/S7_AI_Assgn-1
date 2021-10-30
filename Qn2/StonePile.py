dd62b3668d4a80801e80a0e6a31ea786dac6f21f
Vasanth289 <vasanth0263@gmail.com>
2021-10-30 23:47:16 +0530

    Added botvsbot

--- Qn2/StonePile.py
+++ Qn2/StonePile.py
@@ -51,29 +51,63 @@ def minimax(game, player):
     value, action = recurse(game.state, player)
     return action
 
-def main():
-    initialState = list(map(int, input('Enter initial state of stone piles: ').split()))
+def main(conf, startPlayer):
+    initialState = list(map(int, input('\nEnter initial state of stone piles: ').split()))
 
     game = Game(initialState)
-    print('Current state: [%d, %d]'%(game.state[0], game.state[1]))
+    if conf == 'y' or conf == 'Y':
+        print('\nHuman vs Bot')
+    else:
+        print('\nBot vs Bot')
+    print('\nCurrent state: [%d, %d]'%(game.state[0], game.state[1]))
 
     while(sum(game.state) > 0):
 
-        action = list(map(int, input("Player 1's turn: ").split()))
+        if (conf == 'y' or conf == 'Y') and startPlayer == 1:
+            action = list(map(int, input("Player 1's turn (pile, stones): ").split()))
+        else:
+            action = minimax(game, startPlayer)
+            print("Player %d removed %d from %s pile" % (startPlayer, action[1], \
+                '1st' if action[0] == 0 else '2nd'))
+
         game.state[action[0]] -= action[1]
-        print('Current state: [%d, %d]'%(game.state[0], game.state[1]))
+        print('\nCurrent state: [%d, %d]'%(game.state[0], game.state[1]))
 
         if sum(game.state) == 0:
-            print("You won!")
+            if (conf == 'y' or conf == 'Y') and startPlayer == 1:
+                print("You won!")
+            else:
+                print("Player %d won!" % startPlayer)
             break
-
-        action = minimax(game, 2)
-        game.state[action[0]] -= action[1]
-        print("Player 2 removed %d from %s pile" % (action[1], \
+        
+        nxtPlayer = 1 if startPlayer == 2 else 2
+        if (conf == 'y' or conf == 'Y') and startPlayer == 2:
+            action = list(map(int, input("Player 1's turn (pile, stones): ").split()))
+        else:
+            action = minimax(game, nxtPlayer)
+            print("Player %d removed %d from %s pile" % (nxtPlayer, action[1], \
             '1st' if action[0] == 0 else '2nd'))
-        print('Current state: [%d, %d]'%(game.state[0], game.state[1]))
+
+        game.state[action[0]] -= action[1]
+        print('\nCurrent state: [%d, %d]'%(game.state[0], game.state[1]))
         if sum(game.state) == 0:
-            print("You Lost:(")
+            if (conf == 'y' or conf == 'Y') and startPlayer == 2:
+                print("You won!")
+            else:
+                print("Player %d won!" % nxtPlayer)
 
 if __name__ == "__main__":
-    main()
+    print('Stone Pile Game\n')
+    print('Game instructions are given README.pdf\n')
+    print(30*'=')
+   
+    conf = input('\nDo you want to play?(y/n)')
+    if conf == 'y' or conf == 'Y':
+        conf_ = input('\nWould you like to go first?(y/n)')
+        if conf_ == 'y' or conf_ == 'Y':
+            startPlayer = 1
+        else:
+            startPlayer = 2
+    else:
+        startPlayer = int(input('\nWho should start first? Player 1 or Player 2: '))
+    main(conf, startPlayer)
